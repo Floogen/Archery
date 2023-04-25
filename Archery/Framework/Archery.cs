de0e82e91bfd17ce7cc7164df14ca227a34d591d
Floogen <31755155+Floogen@users.noreply.github.com>
2023-04-25 13:59:43 -0500

    Start of project

---
+++ Archery/Framework/Archery.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace Archery.Framework
+{
+    public class Archery : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
