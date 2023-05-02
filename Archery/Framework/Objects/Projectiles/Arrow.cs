7498acb0806d9858e27ef78e9d94f5ac100c4dd3
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-02 13:49:19 -0500

    Overrode update and related methods for better control over collision logic

--- Archery/Framework/Objects/Projectiles/Arrow.cs
+++ Archery/Framework/Objects/Projectiles/Arrow.cs
@@ -20,6 +20,124 @@ namespace Archery.Framework.Objects.Projectiles
             base.yVelocity.Value /= 4f;
         }
 
+        public override bool update(GameTime time, GameLocation location)
+        {
+            if (Game1.IsMasterGame && base.hostTimeUntilAttackable > 0f)
+            {
+                base.hostTimeUntilAttackable -= (float)time.ElapsedGameTime.TotalSeconds;
+                if (base.hostTimeUntilAttackable <= 0f)
+                {
+                    base.ignoreMeleeAttacks.Value = false;
+                    base.hostTimeUntilAttackable = -1f;
+                }
+            }
+
+            // TODO: Handle light as a property (lightId is private, may want local value)
+            /*
+            if ((bool)base.light)
+            {
+                if (!base.hasLit)
+                {
+                    base.hasLit = true;
+                    base.lightID = Game1.random.Next(int.MinValue, int.MaxValue);
+                    Game1.currentLightSources.Add(new LightSource(4, base.position + new Vector2(32f, 32f), 1f, new Color(0, 65, 128), base.lightID, LightSource.LightContext.None, 0L));
+                }
+                else
+                {
+                    _ = (Vector2)base.position;
+                    Utility.repositionLightSource(base.lightID, base.position + new Vector2(32f, 32f));
+                }
+            }
+            */
+
+            base.rotation += base.rotationVelocity.Value;
+            base.travelTime += time.ElapsedGameTime.Milliseconds;
+            if (base.scaleGrow.Value != 0f)
+            {
+                base.localScale += base.scaleGrow.Value;
+            }
+
+            Vector2 old_position = base.position.Value;
+            base.updatePosition(time);
+
+            // TODO: Reimplement arrow trail / tail logic
+            //base.updateTail(time);
+
+            base.travelDistance += (old_position - base.position.Value).Length();
+            if (base.maxTravelDistance.Value >= 0)
+            {
+                if (base.travelDistance > base.maxTravelDistance.Value - 128)
+                {
+                    // TODO: Implement fading arrows
+                    //base.startingAlpha = ((float)(int)base.maxTravelDistance - base.travelDistance) / 128f;
+                }
+
+                if (base.travelDistance >= base.maxTravelDistance.Value)
+                {
+                    if (base.hasLit)
+                    {
+                        // TODO: Handle removing light source
+                        //Utility.removeLightSource(base.lightID);
+                    }
+
+                    return true;
+                }
+            }
+
+            if (this.isColliding(location) && (base.travelTime > 100 || base.ignoreTravelGracePeriod.Value))
+            {
+                if (base.bouncesLeft.Value <= 0)
+                {
+                    return this.behaviorOnCollision(location);
+                }
+
+                base.bouncesLeft.Value--;
+                bool[] array = Utility.horizontalOrVerticalCollisionDirections(this.getBoundingBox(), base.theOneWhoFiredMe.Get(location), projectile: true);
+                if (array[0])
+                {
+                    base.xVelocity.Value = 0f - base.xVelocity.Value;
+                }
+                if (array[1])
+                {
+                    base.yVelocity.Value = 0f - base.yVelocity.Value;
+                }
+            }
+
+            return false;
+        }
+
+        public bool behaviorOnCollision(GameLocation location)
+        {
+            if (base.hasLit)
+            {
+                // TODO: Handle removing light source
+                //Utility.removeLightSource(base.lightID);
+            }
+
+            foreach (Vector2 tile in Utility.getListOfTileLocationsForBordersOfNonTileRectangle(this.getBoundingBox()))
+            {
+                if (location.terrainFeatures.ContainsKey(tile) && !location.terrainFeatures[tile].isPassable())
+                {
+                    base.behaviorOnCollisionWithTerrainFeature(location.terrainFeatures[tile], tile, location);
+                    return true;
+                }
+
+                if (base.damagesMonsters.Value)
+                {
+                    NPC i = location.doesPositionCollideWithCharacter(this.getBoundingBox());
+                    if (i is not null && i.IsMonster)
+                    {
+                        base.behaviorOnCollisionWithMonster(i, location);
+                        return true;
+                    }
+                }
+            }
+
+            // Note: behaviorOnCollisionWithOther handles collisions with walls / barriers, will want to override
+            base.behaviorOnCollisionWithOther(location);
+            return true;
+        }
+
         public override void behaviorOnCollisionWithMonster(NPC n, GameLocation location)
         {
             base.behaviorOnCollisionWithMonster(n, location);
