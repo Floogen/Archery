d29a6da1025ae5413778da0452c001428e5827f5
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-03 11:32:35 -0500

    Implemented RandomRange.Get

--- Archery/Framework/Models/Generic/RandomRange.cs
+++ Archery/Framework/Models/Generic/RandomRange.cs
@@ -1,8 +1,15 @@
-﻿namespace Archery.Framework.Models.Generic
+﻿using System;
+
+namespace Archery.Framework.Models.Generic
 {
     public class RandomRange
     {
         public int Min { get; set; }
         public int Max { get; set; }
+
+        internal int Get(Random random)
+        {
+            return random.Next(Min, Max + 1);
+        }
     }
 }
