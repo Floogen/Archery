6d24fd849bbfe145052b7b51cf9c208f9c343334
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-03 16:54:41 -0500

    Renamed SpriteModel to ItemSpriteModel

--- Archery/Framework/Models/UI/ItemSpriteModel.cs
+++ Archery/Framework/Models/UI/ItemSpriteModel.cs
@@ -2,7 +2,7 @@
 
 namespace Archery.Framework.Models.Display
 {
-    public class SpriteModel
+    public class ItemSpriteModel
     {
         public Rectangle Source { get; set; }
         public float Scale { get; set; } = 4f;
