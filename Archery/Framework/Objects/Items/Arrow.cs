d4e5232100ab12f719370bc5e87cea6f76e52fb8
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-08 22:34:26 -0500

    Fixed bolts being stackable with arrows and vice versa

--- Archery/Framework/Objects/Items/Arrow.cs
+++ Archery/Framework/Objects/Items/Arrow.cs
@@ -1,4 +1,5 @@
-﻿using Archery.Framework.Models.Weapons;
+﻿using Archery.Framework.Models.Enums;
+using Archery.Framework.Models.Weapons;
 using Archery.Framework.Utilities;
 using Object = StardewValley.Object;
 
@@ -7,10 +8,11 @@ namespace Archery.Framework.Objects.Items
     internal class Arrow : InstancedObject
     {
         private const int ARROW_BASE_ID = 590;
+        private const int BOLT_BASE_ID = 669;
 
         public static Object CreateInstance(AmmoModel ammoModel, int stackCount = 1)
         {
-            var arrow = new Object(ARROW_BASE_ID, stackCount);
+            var arrow = new Object(ammoModel.Type is AmmoType.Arrow ? ARROW_BASE_ID : BOLT_BASE_ID, stackCount);
             arrow.modData[ModDataKeys.AMMO_FLAG] = ammoModel.Id;
 
             return arrow;
