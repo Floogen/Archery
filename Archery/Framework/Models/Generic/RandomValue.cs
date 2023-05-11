ae7a8caee6d065940a18122ab7f8ee56876bb6c1
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-10 21:34:57 -0500

    Implemented RandomValue

---
+++ Archery/Framework/Models/Generic/RandomValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Archery.Framework.Models.Generic
+{
+    public class RandomValue
+    {
+        public List<object> Values { get; set; }
+
+        public T Get<T>(Random random)
+        {
+            if (Values is null || Values.Count == 0)
+            {
+                return default(T);
+            }
+
+            return (T)Convert.ChangeType(Values[random.Next(Values.Count)], typeof(T));
+        }
+    }
+}
