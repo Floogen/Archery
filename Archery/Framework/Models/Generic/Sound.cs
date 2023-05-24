0115e5e8b04a8afdcea0b729d5446cf9740111bd
Floogen <31755155+Floogen@users.noreply.github.com>
2023-05-23 20:03:05 -0500

    Fixed Sound.PitchRandomness not being usable

--- Archery/Framework/Models/Generic/Sound.cs
+++ Archery/Framework/Models/Generic/Sound.cs
@@ -4,11 +4,11 @@ using System;
 
 namespace Archery.Framework.Models.Generic
 {
-    public class Sound : ISound
+    public class Sound
     {
         public string Name { get; set; }
         public int Pitch { get; set; } = -1;
-        public IRandomRange PitchRandomness { get; set; }
+        public RandomRange PitchRandomness { get; set; }
         private float _volume { get; set; } = 1f;
         public float Volume { get { return _volume; } set { _volume = Utility.Clamp(value, 0f, 1f); } }
         public float MaxDistance { get; set; } = 1024f;
